b43734da63887ce71a1552f3810ae4d37f8d1bc8
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-08 06:46:53 -0800

    Created a c file that prints using printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ *
+ * Return: 0 always if success
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
