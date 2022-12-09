289df16bd6323d26dd33b905e5d761af370d19df
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-09 12:56:47 -0800

    It worked hurray

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function of the program
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
