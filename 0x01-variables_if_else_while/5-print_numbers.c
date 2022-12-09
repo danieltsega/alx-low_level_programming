5eef772aee77b7a8ad80d83b4fcd2c8fb118628d
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-09 12:44:02 -0800

    Created a program that prints single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function of the program
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
