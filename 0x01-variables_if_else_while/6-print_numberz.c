051940dc401774a8d05ecdfc83ac0351209577e6
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-09 12:52:33 -0800

    Created a program that prints all single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function of the progam
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
