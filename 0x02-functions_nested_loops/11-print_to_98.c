9811852c5160442924a4e34d2484b5de3e3cb904
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-13 04:55:17 -0800

    Done with the twelveth task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ *
+ * @n: input number
+ *
+ * Return: no return.
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			_putchar("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			_putchar("%d, ", n);
+		}
+	}
+	_putchar("%d\n", n);
+}
