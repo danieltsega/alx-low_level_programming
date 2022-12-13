16008ccedea9471031d2b8c7e6e409b1d024ea9f
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-13 03:15:03 -0800

    First task here we go

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - the main function
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	int msg[] = {95, 112, 117, 116, 99, 104, 97, 114}
+	int c, s;
+
+	s = sizeof(msg) / sizeof(int);
+	for (c = 0; c < s; c++)
+	{
+		_putchar(msg[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
