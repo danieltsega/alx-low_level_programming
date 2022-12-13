58a0db5525a4a7e62a363f78fc7fa38c3d868e10
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-13 03:29:51 -0800

    Created the second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet.
+ *
+ * Return: 0 if Success
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 97; j <= 122; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
