eb69c5836d890a71e6017abc0da8f5ce4d1fa5fd
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-15 02:22:20 -0800

    Created a program the prints the number between 0 and 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number between 0 to 9
+ * Return: has no return
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
