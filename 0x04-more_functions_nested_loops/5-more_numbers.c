5a6c612f0f38514b392df1f1afc5ae843e74c019
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-16 10:06:46 -0800

    Another commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,7 @@ void more_numbers(void)
 		for (ch = 0; ch < 15; ch++)
 		{
 			if (ch >= 10)
-				_putchar((ch / 10) + 49);
+				_putchar((ch / 10) + 48);
 			_putchar((ch % 10) + 48);
 		}
 		_putchar('\n');
