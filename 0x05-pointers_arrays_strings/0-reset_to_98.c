9fe31eaefbaa7daabded2e880630a0f70ff955de
DannySanchez6658 <danieltsega6658@gmail.com>
2022-12-19 12:56:17 -0800

    The task 0 done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and update its value
+ * @n: input
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 on success and -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
