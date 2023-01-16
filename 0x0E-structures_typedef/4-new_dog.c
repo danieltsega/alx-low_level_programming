63a2cf64b528afff699614bc4f46610f2e3698e4
DannySanchez6658 <danieltsega6658@gmail.com>
2023-01-16 11:29:16 -0800

    Done with the task 4 after all this struggle

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,15 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
+
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	return (i);
 }
 
@@ -29,11 +32,12 @@ int _strlen(char *s)
 *
 * Return: the pointer to dest
 */
-
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
+
 
 	while (src[len] != '\0')
 	{
@@ -44,6 +48,7 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
 
@@ -59,21 +64,28 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+
 	int len1, len2;
 
 	len1 = _strlen(name);
+
 	len2 = _strlen(owner);
+
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
+
 	dog->name = malloc(sizeof(char) * (len1 + 1));
+
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
+
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
+
 	if (dog->owner == NULL)
 	{
 		free(dog);
@@ -81,7 +93,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	_strcpy(dog->name, name);
+
 	_strcpy(dog->owner, owner);
+
 	dog->age = age;
 	return (dog);
 
