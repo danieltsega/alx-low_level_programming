9f91e9667ae3a3585c86b7a2ed891d28bb0044c8
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-09 13:17:22 -0700

    [Fix]: Fixed some issues on the node array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_node_t **array;
+	
+	array = malloc(sizeof(hash_node_t *) *size);
+	
+	if(array == NULL)
+	{
+		return(NULL);
+	}
 
 	table->size = size;
-	table->array = malloc(size, sizeof(array *));
+	table->array = array;
 
 	return (table);
 
