221f8da97e37d9ac21b55a48f980f76c7b81f195
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-12 17:31:41 -0700

    [Fix]: Fixed the return value oof the function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
  * Return: a new node
  */
 
-hash_node_t* new_node(char *key, char *value)
+hash_node_t* new_node(const char *key, const char *value)
 {
 	hash_node_t *node = malloc(sizeof(hash_node_t));
 	node->key = strdup(key);
@@ -54,5 +54,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (1);
 	}		
 	
-	return (node);
 }
